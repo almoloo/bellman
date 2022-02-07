55b8436ade4a9aa36b5c9d7d4a08dedd9810cd60
Ali Mousavi <amousavig@icloud.com>
2022-02-07 23:06:11 +0330

    Added the back-end.

---
+++ routes/api.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const router = express.Router();
+
+// ADD ALERT
+router.post('/addAlert', async (req, res) => {
+    const alertData = {
+        id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+        coin: req.body.coin,
+        priceType: req.body.priceType,
+        price: req.body.price,
+        alertType: req.body.alertType,
+        alertTarget: req.body.alertTarget,
+        status: 'active',
+        createDate: new Date().toUTCString()
+    }
+    // SAVE ALERT TO DATABASE
+    flatfile.db('./database.json', async (err, data) => {
+        if(err) {
+            res.status(500).json({
+                success: false,
+                message: 'Error saving alert.'
+            });
+        };
+        data.alerts.push(alertData);
+        data.save();
+        res.json({
+            success: true,
+            message: 'Alert saved.',
+            data: alertData
+        });
+    });
+});
+
+// GET ALERT
+router.post('/getAlert', async (req, res) => {
+    const alertId = req.body.alertId;
+    // GET ALERT FROM DATABASE
+    flatfile.db('./database.json', async (err, data) => {
+        if(err) {
+            res.status(500).json({
+                success: false,
+                message: 'Error getting alert.'
+            });
+        };
+        const alert = data.alerts.find(alert => alert.id === alertId);
+        res.json({
+            success: true,
+            message: 'Alert retrieved.',
+            data: alert
+        });
+    });
+});
+
+// GET ALERTS
+router.post('/getAlerts', async (req, res) => {
+    // GET ALERTS FROM DATABASE
+    flatfile.db('./database.json', async (err, data) => {
+        if(err) {
+            res.status(500).json({
+                success: false,
+                message: 'Error getting alerts.'
+            });
+        };
+        const alerts = data.alerts;
+        res.json({
+            success: true,
+            message: 'Alerts retrieved.',
+            data: alerts
+        });
+    });
+});
+
+// CANCEL ALERT
+router.post('/cancelAlerts', async (req, res) => {
+    const alertId = req.body.alertId;
+    // GET ALERT FROM DATABASE
+    flatfile.db('./database.json', async (err, data) => {
+        if(err) {
+            res.status(500).json({
+                success: false,
+                message: 'Error cancelling alert.'
+            });
+        };
+        const alert = data.alerts.find(alert => alert.id === alertId);
+        alert.status = 'cancelled';
+        data.save();
+        res.json({
+            success: true,
+            message: 'Alert cancelled.',
+            data: alert
+        });
+    });
+});
+
+module.exports = router
