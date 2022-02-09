2346eecccb77bdb8bf2e9a2dfcfb33de0e6e9203
Ali Mousavi <amousavig@icloud.com>
2022-02-10 01:39:46 +0330

    Added email.

--- index.js
+++ index.js
@@ -33,17 +33,45 @@ const sendAlert = (alertId) => {
         if(alertType === 'email') {
             // SEND EMAIL
             const transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: process.env.MAIL_HOST,
+                port: process.env.MAIL_PORT,
+                tls: true,
                 auth: {
-                    user: process.env.EMAIL_USER,
-                    pass: process.env.EMAIL_PASS
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASSWORD
                 }
             });
             const mailOptions = {
-                from: '' + process.env.EMAIL_USER,
+                from: 'bellman <alert@bellman.top>',
                 to: alert.alertTarget,
-                subject: 'Alert',
-                text: 'Your alert has been triggered.'
+                subject: `Your ${alert.coin} alert has been triggered!`,
+                html: ```
+                <!doctype html>
+                <html style="font-size: 16px;">
+                  <head>
+                    <meta name="viewport" content="width=device-width">
+                    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                    <title>bellman - Get notified of cryptocurrency price changes.</title>
+                    <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@400;800&display=swap" rel="stylesheet">
+                  </head>
+                  <body style="background-color: #161f33; font-family: 'Crimson Pro', serif;">
+                    <div class="header" style="text-align: center; padding: 2rem 0;">
+                        <a href="https://bellman.top" class="title-link" style="color: #6abe83;">
+                            <h1 class="title-heading" style="color: #fff; margin: 0; font-weight: bolder;">bellman</h1>
+                        </a>
+                        <small class="subtitle" style="display: block; margin-top: 1rem; color: #aeaeae;">
+                            Get notified of cryptocurrency price changes.
+                        </small>
+                    </div>
+                    <div class="main" style="text-align: center; background-color: #588133; color: #fff; max-width: 90%; margin: auto; border-radius: .5rem; padding: 2rem 0;">
+                        ${alert.coin} price is now ${alert.priceType} <strong>$${alert.price}</strong>.
+                    </div>
+                    <div class="footer" style="text-align: center; color: #aeaeae; padding: 2rem 0;">
+                        This is an automated message from <a href="https://bellman.top" style="color: #6abe83;">bellman.top</a>
+                    </div>
+                  </body>
+                </html>                
+                ```
             };
             transporter.sendMail(mailOptions, function(error, info) {
                 if (error) {
